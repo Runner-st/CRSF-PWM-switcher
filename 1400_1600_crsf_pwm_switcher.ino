bf9d4d0381692cded2aebdeb84796d209c15dcf7
Runner <m36369682@gmail.com>
2024-12-18 13:14:28 +0200

    Excluded serial monitor init for debugging. Set the desired ch8 span through variables at the top of the file

--- 1400_1600_crsf_pwm_switcher.ino
+++ 1400_1600_crsf_pwm_switcher.ino
@@ -6,9 +6,13 @@
 #define PIN_SERIAL2_RX (9u)
 #define PWM_PIN 2    // PWM output pin for sending signal
 
+int spanStart = 1400;
+int spanEnd = 1600;
+
 CrsfSerial crsf(Serial2, CRSF_BAUDRATE);
+
 void setup() {
-  Serial.begin(115200);
+  // Serial.begin(115200);
   Serial2.begin(CRSF_BAUDRATE);
   crsf.begin();
   crsf.onPacketChannels = &packetChannels;
@@ -21,8 +25,8 @@ void loop() {
 
 void packetChannels() {
   int Ch8Value = crsf.getChannel(8);
-  // Check if Ch8 value is between 1400 and 1600
-  if (Ch8Value >= 1400 && Ch8Value <= 1600) {
+  // Check if Ch8 value is within the desired span
+  if (Ch8Value >= spanStart && Ch8Value <= spanEnd) {
       analogWrite(PWM_PIN, 255);
   } else {
       analogWrite(PWM_PIN, 0);
